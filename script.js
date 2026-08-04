279f33879dd9a09ac66d2d963e25ac127f2dae59
P <Proxy0224@outlook.com>
2026-08-04 16:20:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-<div id="article-list">
+fetch("data/articles.json")
+.then(response => response.json())
+.then(data => {
 
-</div>
+    let list = document.getElementById("article-list");
+
+
+    data.forEach(article => {
+
+        let div = document.createElement("div");
+
+        div.className = "article";
+
+
+        div.innerHTML = `
+
+        <a href="articles/${article.file}">
+        ${article.id.toString().padStart(3,"0")} · ${article.title}
+        </a>
+
+
+        <p>
+        ${article.description}
+        </p>
+
+
+        <p>
+        作者：${article.author}
+        时间：${article.time}
+        </p>
+
+        `;
+
+
+        list.appendChild(div);
+
+    });
+
+});
